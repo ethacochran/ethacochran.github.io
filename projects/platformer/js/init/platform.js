5e93993df0e464c7a1d26a1cc0399f18784100f1
ethacochran <ethan.macrae.cochran@gmail.com>
2021-06-09 16:58:48 +0000

    add cannons to game

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -37,11 +37,12 @@
         createPlatform(0, game.world.height - 32, 3, 2);    // DO NOT DELETE
 
         // example:
-        createPlatform(400, 250);
-        createPlatform(250, 590);
+        createPlatform(400, 250, 1, 0.5);
+        createPlatform(250, 590, 1, 0.5);
         createPlatform(250, 460, 0.5, 0.5);
         createPlatform(510, 510, 0.8, 0.5);
         createPlatform(300, 375, 1, 0.5)
+        createPlatform(25, 450, 0.5, 0.4)
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
     }
