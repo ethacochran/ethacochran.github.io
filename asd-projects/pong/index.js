174a3764736bcdbaf78d26bd6a00700be9b65c63
ethacochran <ethan.macrae.cochran@gmail.com>
2021-12-14 00:49:33 +0000

    saving pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -12,7 +12,19 @@ function runProgram(){
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   
   // Game Item Objects
-
+  function gameItemMaker (elementId) {
+    var gameItem = {}
+      gameItem.id = elementId;
+      gameItem.x = parseFloat($(elementId).css('left'));
+      gameItem.y = parseFloat($(elementId).css('top'))
+      gameItem.width = $(elementId).width()
+      gameItem.height = $(elementId).height()
+      gameItem.speedX = 0;
+      gameItem.speedY = 0;
+      
+      return gameItem;
+    
+  }
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
@@ -35,7 +47,19 @@ function runProgram(){
   Called in response to events.
   */
   function handleEvent(event) {
-
+    $(document).on('keydown', moveDown)
+    function moveDown () {
+      if (event.which === KeyboardEvent.keydown ) {
+        gameItem.speedY = -5
+      }
+    }
+    $(document).on('keyup', moveUp)
+    function moveUp () {
+      if (event.which === KeyboardEvent.keyup) {
+        gameItem.speedX = +5
+      }
+    }
+    //Add handler function to move up (moveUp should be name of function)
   }
 
   ////////////////////////////////////////////////////////////////////////////////
